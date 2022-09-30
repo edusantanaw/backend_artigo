877953e167df3887d168eb4b096d1497580be1a5
edusantanaw <102832370+edusantanaw@users.noreply.github.com>
2022-09-29 22:47:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,24 +4,16 @@ const app = express()
 const articles = require('./routes/articleRoutes')
 const user = require('./routes/userRoutes')
 
-
-const myOrigin = 'https://sistema-de-artigos.netlify.app'
-app.use((req, res, next)=> {
-    res.header("Access-Control-Allow-Methods", "GET,OPTIONS, POST, PUT, DELETE")
-    res.header("Access-Control-Allow-Headers", "*")
-    app.use(cors({credentials: true, origin: `${myOrigin}`}));
-    next();
-
-})
-app.use(express.urlencoded({extended: true}))
+app.use(cors({ credentials: true, origin:  "https://sistema-de-artigos.netlify.app"}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static('public'))
 app.use('/article', articles)
 app.use('/user', user)
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('Programa iniciando...')
 })
