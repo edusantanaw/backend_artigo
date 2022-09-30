cb5717ec934b493078209225780268dbc7aa2650
edusantanaw <102832370+edusantanaw@users.noreply.github.com>
2022-09-29 22:33:31 -0300

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://edusantanaw:eduardo098@cluster0.syp9xzq.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://edusantanaw:eduardo098@cluster0.syp9xzq.mongodb.net/?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
     .then(
         console.log('Conectado com sucesso ao MongoDB!')
     ).catch((err) => {
