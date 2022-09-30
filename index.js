a58de9f6ba6200071ea00149cfd1619613ec7b40
edusantanaw <102832370+edusantanaw@users.noreply.github.com>
2022-09-29 22:11:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const user = require('./routes/userRoutes')
 
 app.use((req, res, next)=> {
     res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+    res.header("Access-Control-Allow-Methods", "GET,OPTIONS, POST, PUT, DELETE")
     res.header("Access-Control-Allow-Headers", "*")
     app.use(cors());
     next();
